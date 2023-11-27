e5e52af34cab3d7af4c7364a0b380d4e6636f807
Artem <atremat@gmail.com>
2023-11-28 00:14:35 +0200

    done

---
+++ js/task-3.js
@@ -0,0 +1,95 @@
+/*
+Напиши стрілочну функцію sortByDescendingFriendCount(users) , яка прийматиме один параметр users — масив об’єктів користувачів.
+
+Функція має повертати масив усіх користувачів, відсортованих за спаданням кількостій їх друзів (властивість friends).
+*/
+// розгорнута форма:
+// const sortByDescendingFriendCount = users => {
+//     const usersSorted = users.toSorted(
+//         (first, second) => second.friends.length - first.friends.length,
+//     );
+//     return usersSorted;
+// };
+
+// коротка форма функції:
+const sortByDescendingFriendCount = users =>
+    users.toSorted(
+        (first, second) => second.friends.length - first.friends.length,
+    );
+
+console.log(
+    sortByDescendingFriendCount([
+        {
+            name: 'Moore Hensley',
+            friends: ['Sharron Pace'],
+            gender: 'male',
+        },
+        {
+            name: 'Sharlene Bush',
+            friends: ['Briana Decker', 'Sharron Pace'],
+            gender: 'female',
+        },
+        {
+            name: 'Ross Vazquez',
+            friends: ['Marilyn Mcintosh', 'Padilla Garrison', 'Naomi Buckner'],
+            gender: 'male',
+        },
+        {
+            name: 'Elma Head',
+            friends: ['Goldie Gentry', 'Aisha Tran'],
+            gender: 'female',
+        },
+        {
+            name: 'Carey Barr',
+            friends: ['Jordan Sampson', 'Eddie Strong'],
+            gender: 'male',
+        },
+        {
+            name: 'Blackburn Dotson',
+            friends: ['Jacklyn Lucas', 'Linda Chapman'],
+            gender: 'male',
+        },
+        {
+            name: 'Sheree Anthony',
+            friends: ['Goldie Gentry', 'Briana Decker'],
+            gender: 'female',
+        },
+    ]),
+);
+// [
+//   {
+//     name: "Ross Vazquez",
+//     friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
+//     gender: "male"
+//   },
+//   {
+//     name: "Sharlene Bush",
+//     friends: ["Briana Decker", "Sharron Pace"],
+//     gender: "female"
+//   },
+//   {
+//     name: "Elma Head",
+//     friends: ["Goldie Gentry", "Aisha Tran"],
+//     gender: "female"
+//   },
+//   {
+//     name: "Carey Barr",
+//     friends: ["Jordan Sampson", "Eddie Strong"],
+//     gender: "male"
+//   },
+//   {
+//     name: "Blackburn Dotson",
+//     friends: ["Jacklyn Lucas", "Linda Chapman"],
+//     gender: "male"
+//   },
+//   {
+//     name: "Sheree Anthony",
+//     friends: ["Goldie Gentry", "Briana Decker"],
+//     gender: "female"
+//   },
+//   {
+//     name: "Moore Hensley",
+//     friends: ["Sharron Pace"],
+//     gender: "male"
+//   }
+// ]
